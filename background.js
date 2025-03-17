f96b8ec3175683ecf37ce39c2f9fd50755ee18d6
Sapreet Singh <Sapreet.Singh@autviz.in>
2025-03-17 16:51:20 +0530

    remove consolo.log

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("LinkedIn Auto Commenter Installed!");
-});
-
 // chrome.action.onClicked.addListener((tab) => {
 //     chrome.scripting.executeScript({
 //         target: { tabId: tab.id },
