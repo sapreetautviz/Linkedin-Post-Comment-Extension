9b163d57e9bcd5e1b9c71de84e377156f0472861
Sapreet Singh <Sapreet.Singh@autviz.in>
2025-03-17 15:27:44 +0530

    change permissions or change path of file.

--- background.js
+++ background.js
@@ -2,9 +2,18 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("LinkedIn Auto Commenter Installed!");
 });
 
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["content.js"]
-    });
+// chrome.action.onClicked.addListener((tab) => {
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         files: ["content.js"]
+//     });
+// });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete" && tab.url.includes("/posts/")) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["comment.js"]
+        });
+    }
 });
